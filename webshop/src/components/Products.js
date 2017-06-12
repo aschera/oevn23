1b9bbd7eba46b40cdb0278e8335fd252c2ed5a51
C.B <aschera@gmail.com>
2017-06-12 15:00:20 +0200

    Add files via upload

--- webshop/src/components/Products.js
+++ webshop/src/components/Products.js
@@ -5,33 +5,56 @@ var inputs = {name: undefined, price: undefined, amount: undefined, imgurl: unde
 function Products(props) {
   let i=0;
     const list = props.products.map( x => <li key={i++}>
-      <span>Name: {x.name}</span>,
-      <span> Price: {x.price}</span>,
-      <span> Amount: {x.amount}</span>
-      <span><img src={x.imgurl} width='50' height='50' alt={x.name}/></span>
-      <button onClick={props.handleClickRemoveProduct} id={x.name+x.price}  >delete</button>
+
+      <table class="users">
+      <thead>
+        <tr>
+          <th className="row-1 row-name">Name</th>
+          <th className="row-4 row-img"></th>
+          <th className="row-2 row-price">Price</th>
+          <th className="row-3 row-amount">Amount</th>
+          <th className="row-4 row-button"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td ><input className="edit" value={x.name}/></td>
+          <td ><input className="edit"  value={x.imgurl}/></td>
+          <td ><input className="edit"  value={x.price}/></td>
+          <td id="center"><input className="edit"  value={x.amount}/></td>
+
+          <td ><button onClick={props.handleClickRemoveProduct} id={x.name+x.price}  >delete</button></td>
+          <td ><button onClick={editProduct} id={x.name+x.price}  >edit</button></td>
+        </tr>
+        </tbody>
+      </table>
       </li> )
       return (
   			<div id="ul">
         <h4 className="admin">Products in the store:</h4>
-  			<ul>{list}</ul>
-        <br />
-        <hr />
-        <br  />
-        <h4>Add a new product:</h4>
-        <input type="text" onChange={enterName} className="name" placeholder="name" name="name" />
-        <input type="number" onChange={enterPrice} className="price" placeholder="price" name="price" />
-        <input type="number" onChange={enterAmount} className="amount" placeholder="amount" name="amount" />
-        <input type="text" onChange={enterURL} className="url" placeholder="url" name="url" />
+  			<ul id="alist">{list}</ul>
+
+        <hr className="admin"/>
+
+        <h4 className="admin">Add a new product:</h4>
+        <input type="text" onChange={enterName} className="name" placeholder="name" name="name"  value={inputs.name}/>
+        <input type="number" onChange={enterPrice} className="price" placeholder="price" name="price" value={inputs.price}/>
+        <input type="number" onChange={enterAmount} className="amount" placeholder="amount" name="amount" value={inputs.amount}/>
+        <input type="text" onChange={enterURL} className="url" placeholder="url" name="url" value={inputs.imgurl}/>
         <button type="reset" onClick={AddProduct}>Add product</button>
   		  </div>
   		);
 
 
+      function editProduct(e) {
+        console.log('click');
+      
+      }
 
       function AddProduct(e) {
         props.handleClickAddProduct(e, inputs);
         console.log(inputs);
+        inputs = {name: undefined, price: undefined, amount: undefined, imgurl: undefined}
       }
       function enterName(e){
       	inputs.name = e.target.value;
--- webshop/src/components/Shop.js
+++ webshop/src/components/Shop.js
@@ -5,11 +5,28 @@ import React from 'react';
 function Shop(props) {
   let i=0;
     const list = props.products.map( x => <li key={i++}>
-      <span>Name: {x.name} </span>,
-      <span> Price: {x.price} </span>,
-      <span> Amount: {x.amount} </span>
-      <span><img src={x.imgurl} width='50' height='50' alt={x.name}/></span>
-      <button onClick={click} id={x.name+x.price} >Add to cart</button>
+
+      <table class="users">
+      <thead>
+        <tr>
+          <th className="row-1 row-name">Name</th>
+          <th className="row-4 row-img"></th>
+          <th className="row-2 row-price">Price</th>
+          <th className="row-3 row-amount">Amount</th>
+          <th className="row-4 row-button"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td >{x.name}</td>
+          <td ><img src={x.imgurl} width='50' height='50' alt={x.name}/></td>
+          <td >{x.price}</td>
+          <td id="center">{x.amount}</td>
+
+          <td ><button onClick={click} id={x.name+x.price} >Buy</button></td>
+        </tr>
+        </tbody>
+      </table>
       </li> )
       return (
   			<div id="ul">
