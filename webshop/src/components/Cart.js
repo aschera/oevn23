3c217eb986cfe8a3a3a709e47e63f58c0134dcfc
C.B <aschera@gmail.com>
2017-06-12 22:33:05 +0200

    Add files via upload

--- webshop/src/components/Cart.js
+++ webshop/src/components/Cart.js
@@ -6,9 +6,22 @@ function Cart(props) {
     let i=0;
     let price = 0;
     const list = props.cart.map( x => <li key={i++}>
-      <span>Name: {x.name} </span>,
-      <span> Price: {x.price} </span>,
-      <span> Amount: {x.amount} </span>
+      <table class="users">
+      <thead>
+        <tr>
+          <th className="row-1 row-name">Name</th>
+          <th className="row-3 row-amount">Amount</th>
+          <th className="row-2 row-price  right">Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td >{x.name}</td>
+          <td >{x.amount}</td>
+          <td className="right">{x.price}</td>
+        </tr>
+        </tbody>
+      </table>
       </li>);
 
       let l = list.length;
@@ -21,8 +34,17 @@ function Cart(props) {
   			<div id="ul">
         <h4 className="customer">The Cart:</h4>
         <h5>You have {l} items</h5>
-  			<ul>{list}</ul>
-        <div>Total amount to pay: <strong> {price} </strong> </div>
+  			<ul>{list}
+
+        <table className="amountsale">
+        <thead>
+          <tr className="amountsale">
+            <th className="left">Total Amount:</th>
+            <th className="right price">{price}</th>
+          </tr>
+        </thead>
+        </table>
+        </ul>
         <button>Checkout</button>
   		  </div>
   		);
